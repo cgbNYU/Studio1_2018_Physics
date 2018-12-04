6ba3c39f2f1a0a1e081d2c4f67c994bb616a5203
cgbNYU <cgb316@nyu.edu>
2018-12-04 11:37:55 -0500

    Adjusted floor size and front slider joint Objects now delete themselves when they fall far enough

---
+++ Assets/Scripts/ObjectCull.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCull : MonoBehaviour {
+	
+	//Remove objects when they leave the screen
+	public float offScreenBuffer;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		OffScreenCull();
+	}
+
+	public void OffScreenCull()
+	{
+		if (transform.position.y < offScreenBuffer)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
