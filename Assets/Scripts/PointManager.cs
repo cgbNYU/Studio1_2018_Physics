ab35e96b2e648b67104fd54214df2b331aa413ec
maryageorgescu <mg3200@nyu.edu>
2018-12-03 20:56:35 -0500

    Added some scripts. One for a restart, two for score keeping.

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointManager : MonoBehaviour
+{
+	
+	public Text pointsText;
+	public int currentPoints = 0;
+	
+
+// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pointsText.text ="Point:"+currentPoints;
+	}
+	
+	public void IncreasePoints(int pointsToAdd)
+	{
+
+		currentPoints = currentPoints + pointsToAdd;
+	}
+	
+}
