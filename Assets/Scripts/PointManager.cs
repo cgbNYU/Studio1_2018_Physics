22cd6615842cbf84daf700fb55985bb0679acfc0
cgbNYU <cgb316@nyu.edu>
2018-12-12 14:35:41 -0500

    Redid combo code to make the glitches go away. Changed radish slider joint

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class PointManager : MonoBehaviour
 {
@@ -21,6 +22,12 @@ public class PointManager : MonoBehaviour
 	public GameObject swordBlade;
 
 	public Text HighScoreText;
+	
+	//Points variables
+	private float lastPoints;
+	private float thisPoints;
+	private float comboPoints;
+	private bool wasPepper;
 	
 	private void Start()
 	{
@@ -49,15 +56,56 @@ public class PointManager : MonoBehaviour
 	//It will also add the letter to the string
 	public void AddFoodToStack(GameObject obj, Char letter)
 	{
+		//Stick things to the stack
 		impaledFoodStack.Push(obj);
 		impaledFoodString += letter;
+
+		//Points stuff
+		ImpaleScript foodStats = obj.GetComponent<ImpaleScript>(); //get the script off the impaled object
+		thisPoints = foodStats.cookPointValue; //add the cook value of the food to the current point holder
+		if (wasPepper) //check if the last object was a pepper, and if it was, multiply thisPoints
+		{
+			thisPoints *= pepperMultiplier;
+		}
+		if (foodStats.isPepper) //check if the current food is a pepper, and if it is set wasPepper to true and multiply the last points
+		{
+			wasPepper = true;
+			lastPoints *= pepperMultiplier;
+		}
+		else //if it is not a pepper, set wasPepper to false
+		{
+			wasPepper = false;
+		}
+
+		comboPoints += lastPoints; //add the previous food's value to the comboPoints
+		lastPoints = thisPoints; //make the current points value the last points value
+		thisPoints = 0; //reset thisPoints just in case
 	}
 	
 	//Call this from the grill
 	//When the timer runs down, give out the combo points and reset the necessary variables
+	//What if I calculate this as it happens?
+	//So When food hits the sword it is added to a stack, and the relevant points values are as well
+	//Every food stores its valu when it is placed on the sword
 	public void ComboPayout()
 	{
-		if (impaledFoodStack.Count != 0)
+		//Points calculations
+		comboPoints += lastPoints; //grab the last food's points value, which will not have been updated
+		IncreasePoints(comboPoints); //increase the points and display to the screen
+		//Reset all of the combo variables
+		comboPoints = 0;
+		lastPoints = 0;
+		thisPoints = 0;
+		wasPepper = false;
+		
+		//Destroy cooked food
+		DestroyCookedFood();
+		//Destroy Slider Joints
+		DestroySliderJoints();
+		
+		
+
+		/*if (impaledFoodStack.Count != 0)
 		{
 			float comboTotal = 0;
 			float comboMultiplier = 1;
@@ -111,8 +159,32 @@ public class PointManager : MonoBehaviour
 
 			comboTotal *= comboMultiplier;
 			IncreasePoints(comboTotal);
-		}
+		}*/
 
+	}
+
+	//When the food is cooked, for loop through the stack and destroy all the foods
+	public void DestroyCookedFood()
+	{
+		//for loop
+		for (int i = 0; i < impaledFoodStack.Count + 1; i++)
+		{
+			GameObject cookedFood = impaledFoodStack.Pop();
+			Destroy(cookedFood);
+		}
+		impaledFoodStack.Clear(); //clear out the stack so it doesn't keep it cooking
+	}
+	
+	//When the cooked food is destroyed, grab all the slider joints on the blade and destroy them
+	public void DestroySliderJoints()
+	{
+		//Grab an array of all the sliders on the blade
+		SliderJoint2D[] oldSliders = swordBlade.GetComponents<SliderJoint2D>();
+		//iterate through and delete the sliders
+		for (int i = 0; i < oldSliders.Length; i++)
+		{
+			Destroy(oldSliders[i]);
+		}
 	}
 
 	//When you click, pop the top object from the stack and delete the fixed joint
